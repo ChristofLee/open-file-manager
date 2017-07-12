4f7ced9f7c9115b2c6bef547bedbcc7fdc052039
Christopher Lee <christopher.lee@jadu.co.uk>
2017-07-12 21:34:30 +0100

    Move my reference URL's into my readme

--- open-file-manager.py
+++ open-file-manager.py
@@ -1,7 +1,3 @@
-# http://docs.sublimetext.info/en/latest/extensibility/plugins.html
-# http://www.sublimetext.com/docs/3/api_reference.html
-# https://learnxinyminutes.com/docs/python3/
-
 import sublime
 import sublime_plugin
 
