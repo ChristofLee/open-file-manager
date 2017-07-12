8059ec127d41f523df9e743ce1cd1c3ce3b2674d
Christopher Lee <christopher.lee@jadu.co.uk>
2017-07-12 22:02:10 +0100

    Update function name

--- open-file-manager.py
+++ open-file-manager.py
@@ -5,3 +5,5 @@ import sublime_plugin
 class ExampleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.insert(edit, 0, "Hello, World!")
+
+# view.run_command('open_file_manager')
