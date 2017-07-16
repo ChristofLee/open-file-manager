199568233e75a7ef40b027025153c2c37812d27f
Christopher Lee <christopher.lee@jadu.co.uk>
2017-07-16 20:58:14 +0100

    Remove old comments

--- open-file-manager.py
+++ open-file-manager.py
@@ -3,20 +3,10 @@ import sublime_plugin
 
 class OpenFileManagerCommand(sublime_plugin.WindowCommand):
     def run(self):
-    # Get a list of all open files
-        openViews = self.window.views()
-        # x = 0
-        # for i in openViews:
-        #     print(openViews[x].file_name())
-        #     x += 1
         openFiles = self.getFilenames()
         sortedFiles = self.sortByFilename(openFiles)
         self.printFilenames(sortedFiles)
 
-    # Re-order those files
-        # Index the list
-        # Sort alphabetically
-        # Apply a new index
     # Get all open files
     def getFilenames(self):
         openFiles = self.window.views()
