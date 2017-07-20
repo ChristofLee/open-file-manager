d6fd4fae99977683caad7865ffe8cee54a871964
Christopher Lee <christopher.lee@jadu.co.uk>
2017-07-20 17:52:16 +0100

    Rewrite class to handle sorting, ordering and index
    - Help from Carl Newton

--- open-file-manager.py
+++ open-file-manager.py
@@ -3,34 +3,98 @@ import sublime_plugin
 
 class OpenFileManagerCommand(sublime_plugin.WindowCommand):
     def run(self):
-        openFiles = self.getFilenames()
-        sortedFiles = self.sortByFilename(openFiles)
-        self.indexByFilename(sortedFiles)
+        open_files = self.get_filenames()
+        grouped_files = self.group_by_group_rules(open_files)
+        ordered_files = self.order_files(grouped_files)
+        self.index_by_filename(ordered_files)
+
+    def group_by_group_rules(self, files):
+        group_rules = {
+            "1": [".php", ".html"],
+            "2": [".css", ".scss"],
+            "3": [".js", ".todo", "opening.php"]
+        }
+        return_dict = {}
+        for file in files:
+            file_placed = False
+            filename = file.file_name()
+            for group, rules in group_rules.items():
+                for rule in rules:
+                    if filename and rule in filename:
+                        if group not in return_dict:
+                            return_dict[group] = []
+                        return_dict[group].append(file)
+                        file_placed = True
+            if file_placed == False:
+                if '4' not in return_dict:
+                    return_dict['4'] = []
+                return_dict['4'].append(file)
+        return return_dict
+
+    def order_files(self, file_dict):
+        return_dict = {}
+        for group, file_list in file_dict.items():
+            if group not in return_dict:
+                return_dict[group] = sorted(
+                    file_list, key=lambda view: view.file_name()
+                )
+        return return_dict
+
+    def get_filename_extension(self, view):
+        filename_array = str(view.file_name()).split('.')
+        return filename_array[-1]
 
     # Get all open files
-    def getFilenames(self):
-        openFiles = self.window.views()
-        return openFiles
+    def get_filenames(self):
+        open_files = self.window.views()
+        return open_files
 
     # Sort files alphabetically
-    def sortByFilename(self, list):
-        sortedFilenames = sorted(list, key=lambda x: x.file_name())
-        return sortedFilenames
-
-    # Sort files alphabetically
-    def indexByFilename(self, list):
-        x = 0
-        for i in list:
-            print(x)
-            self.window.set_view_index(i, x, x)
-            x += 1
-
-    # Display filenames (for debugging purposes)
-    def printFilenames(self, list):
-        for i in list:
-            print(i.file_name())
-
-    # Filter list by file extensions
-        # Index the list
-        # Filter by file extension
-        # Apply a new index
+    def index_by_filename(self, ordered_files):
+        group_count = 0
+        window_count = len(ordered_files)
+        if window_count == 1:
+            layout = {
+                "cols": [0.0, 1.0],
+                "rows": [0.0, 1.0],
+                "cells": [
+                    [0, 0, 1, 1],
+                ]
+            }
+        elif window_count == 2:
+            layout = {
+                "cols": [0.0, 0.5, 1.0],
+                "rows": [0.0, 1.0],
+                "cells": [
+                    [0, 0, 1, 1],
+                    [1, 0, 2, 1],
+                ]
+            }
+        elif window_count == 3:
+            layout = {
+                "cols": [0.0, 0.5, 1.0],
+                "rows": [0.0, 0.5, 1.0],
+                "cells": [
+                    [0, 0, 1, 2],
+                    [1, 0, 2, 1],
+                    [1, 1, 2, 2]
+                ]
+            }
+        else:
+            layout = {
+                "cols": [0.0, 0.5, 1.0],
+                "rows": [0.0, 0.5, 1.0],
+                "cells": [
+                    [0, 0, 1, 1],
+                    [1, 0, 2, 1],
+                    [0, 1, 1, 2],
+                    [1, 1, 2, 2]
+                ]
+            }
+        self.window.run_command('set_layout', layout)
+        for group, files in ordered_files.items():
+            file_count = 0
+            for file in files:
+                self.window.set_view_index(file, group_count, file_count)
+                file_count += 1
+            group_count += 1
